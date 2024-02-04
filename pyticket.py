b96c2a1bdadd180d6da3452fbf3f4a0d2430454a
sully <sully.elhassan@gmail.com>
2024-02-03 19:40:46 -0500

    added register logic, added login logic, added flask_login imports, added flash imports, added flash messaging to html templates, added FlaskForms, added flaskForm logic to html templates

--- pyticket.py
+++ pyticket.py
@@ -2,12 +2,28 @@ from flask import Flask
 from flask import render_template
 from flask import redirect
 from flask import url_for
+from flask import flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_login import UserMixin
+from flask_login import login_user
+from flask_login import login_required
+from flask_login import logout_user
+from flask_login import current_user
+from flask_bcrypt import Bcrypt
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms import PasswordField
+from wtforms import SubmitField
+from wtforms import validators
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users_db.db'
 app.secret_key = "b'\xb2\xa6fR\x0c\x1d\xf8\xe1@\xd5\xe1\xe4\x88\xc8l\xbe'"
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -15,21 +31,80 @@ class User(db.Model):
     email = db.Column(db.String(), unique=True, nullable=False)
     password = db.Column(db.String(), unique=False, nullable=False)
 
-    def __repr__(self):
-        return f"User('{self.username}', '{self.email}', '{self.id}')"
+    def is_authenticated():
+        return True
+    
+    def is_active():
+        return True
 
+    def is_anonymous():
+        return False
+
+    def get_id(self):
+        return str(self.id)
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', [validators.DataRequired()])
+    email = StringField('Email', [validators.DataRequired(), validators.Email()])
+    password = PasswordField('Password', [validators.DataRequired(), validators.Length(min=6)])
+    confirm_password = PasswordField('Confirm Password', [validators.DataRequired(), validators.EqualTo('password')])
+    submit = SubmitField('Register')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', [validators.DataRequired(), validators.Email()])
+    password = PasswordField('Password', [validators.DataRequired()])
+    submit = SubmitField('Login')
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/register/')
+@app.route('/register/', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html')
+    form = RegistrationForm()
 
-@app.route('/login/')
+    if form.validate_on_submit():
+        username = form.username.data
+        email = form.email.data
+        password = form.password.data
+        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+        existing_user = User.query.filter_by(email=email).first()
+
+        if existing_user:
+            flash('Email address is already taken. Please choose a different one.', 'danger')
+            return redirect(url_for('register'))
+
+        new_user = User(username=username, email=email, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash('Your account has been created! You can now log in.', 'success')
+        return redirect(url_for('login'))
+
+    return render_template('register.html', form=form)
+
+@app.route('/login/', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+        user = User.query.filter_by(email=email).first()
+
+        if user and bcrypt.check_password_hash(user.password, password):
+            login_user(user)
+            flash('Login successful!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful. Please check your email and password.', 'danger')
+
+    return render_template('login.html', form=form)
 
 @app.route('/test_db')
 def test_db():
@@ -42,4 +117,3 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     app.run(debug=False)
-
